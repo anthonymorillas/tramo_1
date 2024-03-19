39a645f16cad5c4519c118bdccc9b397b93111e6
anthonymorillas <anthonymorillas@gmail.com>
2024-03-19 09:32:18 -0300

    Se agregan todos los documentos

---
+++ data.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import altair as alt
+
+# @st.cache_data
+def load_data():
+    # Cargar datos y especificar tipo de datos para la columna problemática
+    df = pd.read_excel('PUB_EMPRESAS_DATA_1.xlsx', dtype={'column_name': str}) #nrows=1000
+    return df
+
+data_load_state = st.text('Cargando data...')
+df = load_data()
+data_load_state.text('Cargando data... Hecho!')
+
+st.subheader('Raw data')
+st.write(df)
